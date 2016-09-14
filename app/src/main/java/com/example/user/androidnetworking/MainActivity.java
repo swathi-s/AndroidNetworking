6867c67ade3b417df6221a38e4aebda8226b77d0
swathi-s <swathi.ehr@gmail.com>
2016-09-14 15:49:37 +0530

    Inputstream response is converted into string and shown on the console

--- app/src/main/java/com/example/user/androidnetworking/MainActivity.java
+++ app/src/main/java/com/example/user/androidnetworking/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.user.androidnetworking;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -27,27 +32,72 @@ public class MainActivity extends AppCompatActivity {
 
                 Toast.makeText(getApplicationContext(),"Get Data button has clicked",Toast.LENGTH_SHORT).show();
 
-                String myUrl = "http://192.168.1.7/androidService.php";
-                try {
-                    URL url = new URL(myUrl);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setReadTimeout(10000); // in milliseconds
-                    conn.setConnectTimeout(15000); //in milliseconds
-                    conn.setRequestMethod("GET");
-                    conn.setDoInput(true);
-
-                    // Starts the query
-                    conn.connect();
-                    int response = conn.getResponseCode();
-                    Log.d("response", "The response is: " + response);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                new GetDataFromServer().execute();
 
 
             }
         });
+    }
+
+    class GetDataFromServer extends AsyncTask
+    {
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            InputStream is;
+            String myUrl = "http://192.168.1.7/androidService.php";
+            try {
+                URL url = new URL(myUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d("response","code is "+response);
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+
+                TextView txt = (TextView) findViewById(R.id.textView);
+               // txt.setText(contentAsString);
+
+
+                BufferedReader br = null;
+                StringBuilder sb = new StringBuilder();
+
+                String line;
+                try {
+
+                    br = new BufferedReader(new InputStreamReader(is));
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (br != null) {
+                        try {
+                            br.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                String contentAsString = sb.toString();
+                Log.d("response","data is "+contentAsString);
+
+                return contentAsString;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
